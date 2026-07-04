6ad611594465d81325d40fb7140e0669f76e3637
chantel <nyabokechantel83@gmail.com>
2026-07-04 21:33:38 +0300

    Initial project setup

--- index.js
+++ index.js
@@ -4,3 +4,27 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+require("datejs");
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  // Loop through each array of usernames
+  for (const userArray of args) {
+    // Merge the usernames into the users array
+    combinedObject.users.push(...userArray);
+  }
+
+  // Add today's date in M/d/yyyy format
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Return the completed object
+  return combinedObject;
+}
+
+module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
+};
